711ac5019fe499c2f2dfdf83c1159d74626ef015
Jazthedev <joelpepa2@gmail.com>
2023-05-09 18:50:41 +0200

    add tickets schema

--- thetatix/server/models/ticket.js
+++ thetatix/server/models/ticket.js
@@ -1,8 +1,23 @@
 const mongoose = require('mongoose');
 
 const ticketSchema = new mongoose.Schema({
-    //completar
+    eventContractAdress: {
+        type: String,
+        required: true
+    },
+    owner:{
+        type: String,//adress of wallet owner
+        required: true
+    },
+    ticketNumber: {
+        type: Number,
+        required: true
+    },
+    used: {
+        type: Boolean,
+        default: false
+    }
 })
 
-const Ticket = mongoose.model('ticket',ticketSchema);
+const Ticket = mongoose.models.ticket || mongoose.model('ticket',ticketSchema);
 export default Ticket;
