67e02ebf819561cba4a8980a870f8d9534d2ae7c
Jazthedev <joelpepa2@gmail.com>
2023-05-09 19:09:51 +0200

    fix diego endpoint

--- thetatix/src/pages/api/hello.js
+++ thetatix/src/pages/api/hello.js
@@ -1,15 +1,11 @@
-// import { initialize } from "@/server/mongo";
+import User from "@/server/models/usuario";
+import connectMongo from "@/server/mongo";
 
-// export default async function handler(req, res) {
-//   const db = await initialize();
-//   const collection = db.collection("test");
-//   const result = await collection.insertOne({ hello: "world" });
-//   res.status(200).json({ message: "Inserted document with _id: " + result.insertedId });
-// }
-
-
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+  console.log('CONNECTING TO MONGO');
+    await connectMongo();
+    console.log('CONNECTED TO MONGO');
+    const user = await User.create({walletAddress:"jdhkjfdjhfhjhkfdgddfdfs"})
+    console.log(user)
+  res.status(200).json(user)
 }
