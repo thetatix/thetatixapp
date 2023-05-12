775706b7f68f545fdbfdfe76bc2d79ac19362561
Jazthedev <joelpepa2@gmail.com>
2023-05-12 10:40:09 +0200

    add api getUsedTickets

--- thetatix/src/pages/api/tickets/getUserTickets.js
+++ thetatix/src/pages/api/tickets/getUserTickets.js
@@ -1,4 +1,26 @@
+import Ticket from "@/server/models/ticket"
+import Event from "@/server/models/event";
+import connectMongo from "@/server/mongo";
+
+
 export default async function handler(req, res) {
-    //-----------COMPLETAR-----------
-    res.status(200).json({data:'succes'})
+    await connectMongo();
+    const { owner } = req.query;
+    const userTickets = await Ticket.find({
+        owner:owner
+    }).populate("eventDataTrait");
+    //eventDataTrait is invisible so solve it
+    const array_object_visible_fields = [];
+    //make event trait visible
+    for(let ticket of userTickets){
+        array_object_visible_fields.push({
+            owner:ticket.owner,
+            ticketNumber:ticket.ticketNumber,
+            eventContractAdress:ticket.eventContractAdress,
+            user:ticket.used,
+            usedDate:ticket.usedDate,
+            eventTrait:ticket.eventDataTrait
+        })
+    }
+    res.status(200).json({data:array_object_visible_fields})
 }
