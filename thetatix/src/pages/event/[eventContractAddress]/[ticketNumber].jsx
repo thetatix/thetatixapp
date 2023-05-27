10ab5d23b995cd820f92a0e8aad106e2c7dd8c1c
Esteban Zepeda <estzepeda@outlook.com>
2023-05-26 19:05:46 -0600

    Small fixes, my ticket

--- thetatix/src/pages/event/[eventContractAddress]/[ticketNumber].jsx
+++ thetatix/src/pages/event/[eventContractAddress]/[ticketNumber].jsx
@@ -7,7 +7,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
 import styles from '@/assets/styles/Pages.module.css'
-import styleEvent from '@/assets/styles/Ticket.module.css'
+import styleTicket from '@/assets/styles/Ticket.module.css'
 
 export default function TicketPage() {
   const [ticket, setTicket] = useState({});
@@ -91,37 +91,69 @@ export default function TicketPage() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
+        <header className={styles.header}>
+            <div className={styles.headerContainer + ' container'}>
+                <div className={styles.content + ' row'}>
+                    <div className='col-12'>
+                        <p className={styles.breadcrumbs}>
+                            <Link href="/mytickets">My tickets</Link> / <Link href={"/event/" + ticket.eventContractAdress + "/" + ticket.ticketNumber}>{ticket.ticketNumber}</Link>
+                        </p>
+                        <h1>
+                          {loading ? null : (
+                            <Link href={"/event/" + ticket.eventContractAdress} target='blank'>
+                              {ticket.eventTrait.eventName}
+                              <Image
+                                src="/icons/external-link.svg"
+                                alt="External link icon"
+                                width={32}
+                                height={32}
+                              />
+                            </Link>
+                          )}
+                        </h1>
+                    </div>
+                </div>
+            </div>
+        </header>
         <section className={styles.section}>
           <div className={styles.sectionContainer + ' container'}>
             <div className={styles.content}>
               {loading ? (
                 <p>Loading ticket...</p>
               ) : (
-                <div className={styleEvent.ticket}>
-                  <div className={styleEvent.eventImg}>
+                <div className={styleTicket.ticket}>
+                  <div className={styleTicket.eventImg}>
                       <Image
                           src={bufferToImg(ticket.eventTrait.img)}
                           alt="Event image"
                           width={2400}
                           height={1600}/>
                   </div>
-                  <div className={styleEvent.ticketInfo}>
-                    <div className={styleEvent.title}>
+                  <div className={styleTicket.ticketInfo}>
+                    <div className={styleTicket.title}>
                       <h1>Ticket: {ticket.ticketNumber}</h1>
                     </div>
-                    <div className={styleEvent.creator}>
+                    <div className={styleTicket.creator}>
                         <h2>Owned by</h2>
-                        <span onClick={() => copyToClipboard(ticket.owner)}>
-                            <abbr title={ticket.owner}>{formatAddress(ticket.owner)}</abbr>
-                        </span>
+                        {ticket.owner === address ? (
+                          <span onClick={() => copyToClipboard(ticket.owner)} className={styleTicket.you}>
+                            <abbr title={ticket.owner}>You</abbr>
+                          </span>
+                        ) : (
+                          <span onClick={() => copyToClipboard(ticket.owner)}>
+                            <abbr title={ticket.owner}>
+                              {formatAddress(ticket.owner)}
+                            </abbr>
+                          </span>
+                        )}
                     </div>
-                    <div className={styleEvent.contractAddress}>
+                    <div className={styleTicket.contractAddress}>
                         <h2>Ticket address</h2>
                         <span onClick={() => copyToClipboard(ticket.eventContractAdress)}>
                             <abbr title={ticket.eventContractAdress}>{formatAddress(ticket.eventContractAdress)}</abbr>
                         </span>
                     </div>
-                    <div className={styleEvent.date}>
+                    <div className={styleTicket.date}>
                         <h2>Date and time</h2>
                         <p>
                             Starts {formatDateTime(ticket.eventTrait.startDate)}
@@ -129,19 +161,26 @@ export default function TicketPage() {
                             Ends {formatDateTime(ticket.eventTrait.endDate)}
                         </p>
                     </div>
-                    <div className={styleEvent.location}>
+                    <div className={styleTicket.location}>
                         <h2>Location</h2>
                         <p>{ticket.eventTrait.location}</p>
                     </div>
-                    <div className={styleEvent.description}>
+                    <div className={styleTicket.description}>
                         <p>{formatDescription(ticket.eventTrait.eventDescription)}</p>
                     </div>
 
-                    <div className={styleEvent.ticketLink}>
-                      <p>{ticket.user ? 'Ticket already used' : 'Ticket available'}</p>
+                    <div className={styleTicket.ticketLink}>
+                      <button disabled={ticket.user}>
+                        {ticket.user ? (
+                          'Ticket already used'
+                        ): (
+                          'Ticket avaible'
+                        )}
+                      </button>
                     </div>
-                    <p>Used: {ticket.user ? 'Yes' : 'No'}</p>
-                    {ticket.user && <p>Used Date: {ticket.usedDate}</p>}
+
+                    {/* <p>Used: {ticket.user ? 'Yes' : 'No'}</p>
+                    {ticket.user && <p>Used Date: {ticket.usedDate}</p>} */}
                     
                   </div>
                 </div>
