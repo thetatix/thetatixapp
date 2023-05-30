d7c7c656ffe8aece166c09eedf1c406072e56a7f
Jazthedev <joelpepa2@gmail.com>
2023-05-30 17:54:16 +0200

    improve create event functionality

--- thetatix/src/components/contractsHook/useContract.js
+++ thetatix/src/components/contractsHook/useContract.js
@@ -12,47 +12,47 @@ class useContracts {
     #ABIticket;
     #signer;
 
-    constructor(_signer){
+    constructor(_signer) {
         this.#signer = _signer;
         this.#ABIfactory = ABI_file_factory.abi;
         this.#ABIticket = ABI_file_ticket.abi;
-        this.#factoryContract = new ethers.Contract('0x464610006e627B14cA808b1BF1456308Eda2Ed7A',this.#ABIfactory,_signer);
-    }   
+        this.#factoryContract = new ethers.Contract('0x464610006e627B14cA808b1BF1456308Eda2Ed7A', this.#ABIfactory, _signer);
+    }
 
-    async working(){
+    async working() {
         const data = await this.#factoryContract.working();
-        console.log('data',data);
+        console.log('data', data);
         return data
     }
 
-    async createEventTickets(_creator, _maxTickets, _name, _ticketPrice, _description, _startDate, _img, _endDate, _location, _category,_eventtype,_api_key,_api_secret) {
-        if(_creator.length===0){
+    async createEventTickets(_creator, _maxTickets, _name, _ticketPrice, _description, _startDate, _img, _endDate, _location, _category, _eventtype, _api_key, _api_secret) {
+        if (_creator.length === 0) {
             return { error: "Wallet not connected.", data: null, status: "warning", message: "Wallet not connected." };   //data = address created contract
         }
-        try{
+        try {
             let streamid = "";
             let stream_key = "";
             let stream_server = "";
             let isOnlineEventStream = false;
 
-            if(_eventtype == "online"){
+            if (_eventtype == "online") {
                 //create stream with private keys
                 let created_stream;
                 isOnlineEventStream = true;
-                try{
+                try {
                     created_stream = await axios({
                         method: "POST",
                         url: "https://api.thetavideoapi.com/stream",
                         headers: {
-                        "x-tva-sa-id": _api_key,
-                        "x-tva-sa-secret": _api_secret,
+                            "x-tva-sa-id": _api_key,
+                            "x-tva-sa-secret": _api_secret,
                         },
-                        body:{
-                            name:`thetatix ${_name}`, resolutions:["160p","240p","360p","720p","source"], source_resolution:"720p", "fps":60
+                        body: {
+                            name: `thetatix ${_name}`, resolutions: ["160p", "240p", "360p", "720p", "source"], source_resolution: "720p", "fps": 60
                         }
-                        
+
                     });
-                }catch(err){
+                } catch (err) {
                     return { error: err, data: null, status: "danger", message: "invalid api or secret key, also u may have exceed 3 streams per thetavideoapi private keys" };
                 }
                 //define needed properties to sotre on backend
@@ -64,12 +64,25 @@ class useContracts {
             const contract_uuid = uuidv4();
 
             //create event at blockchain
-            const transaction = await this.#factoryContract.createEvent(_name,_description,_ticketPrice,_maxTickets,Date.now(),contract_uuid);
+            const transaction = await this.#factoryContract.createEvent(_name, _description, _ticketPrice, _maxTickets, Date.now(), contract_uuid);
             transaction.wait();
-            await this.delay(30);
+            // await this.delay(30);
 
             //query the address by uuid
-            const newEvent_address = await this.#factoryContract.getAddressFromUuid(contract_uuid);
+            let newEvent_address = await this.#factoryContract.getAddressFromUuid(contract_uuid);
+            console.log('newEvent_address', newEvent_address)
+            if (newEvent_address = '0x0000000000000000000000000000000000000000') {
+                let i = 0;
+                while (i < 1) {
+
+                    newEvent_address = await this.#factoryContract.getAddressFromUuid(contract_uuid);
+                    if (newEvent_address != '0x0000000000000000000000000000000000000000') {
+                        console.log('fetching', newEvent_address)
+                        i++
+                    }
+                }
+            }
+            console.log('good newEvent_address', newEvent_address)
 
             //push to the db
             const raw_data = {
@@ -100,100 +113,100 @@ class useContracts {
                 body: data
             });
             if (event.status == 413) {
-               return { error: event.statusText, data: null, status: "danger", message: "Image exceeds 1MB limit." };
+                return { error: event.statusText, data: null, status: "danger", message: "Image exceeds 1MB limit." };
             }
             return { error: null, data: event, status: "success", message: "Event created successfully." };   //data = address created contract
-        }catch(err){
+        } catch (err) {
             console.log(err);
-            return {error: err, data: null, status: "danger", message: "There was an error creating your event."}
+            return { error: err, data: null, status: "danger", message: "There was an error creating your event." }
 
         }
-        
-        
+
+
     }
 
-    async buyTicket(buyerWalletAddress,ticketEventAddress,price){
-        if(buyerWalletAddress.length===0){
-            return {error:"please connect ur wallet",data:null};   //data = address created contract
+    async buyTicket(buyerWalletAddress, ticketEventAddress, price) {
+        if (buyerWalletAddress.length === 0) {
+            return { error: "please connect ur wallet", data: null };   //data = address created contract
         }
         console.log('buyin ticket')
         //buy ticket on smart contract
-        const event_contract = new ethers.Contract(ticketEventAddress,this.#ABIticket,this.#signer);
-        try{
-            const transaction = await event_contract.buyTicket({value:ethers.utils.parseEther(price.toString())});
+        const event_contract = new ethers.Contract(ticketEventAddress, this.#ABIticket, this.#signer);
+        try {
+            const transaction = await event_contract.buyTicket({ value: ethers.utils.parseEther(price.toString()) });
             transaction.wait();
             await this.delay(10);
 
             //get user tickets on that smart contract
             const tickets_numbers = await event_contract.getUserTickets(buyerWalletAddress);
             //convert hex array to numbers array
-            const parsedNumbers = tickets_numbers.map(ticket => parseInt(ticket._hex,16));
+            const parsedNumbers = tickets_numbers.map(ticket => parseInt(ticket._hex, 16));
             //add ticket to database 
             const raw_data = {
-                eventContractAdress:ticketEventAddress,
+                eventContractAdress: ticketEventAddress,
                 userTickets: parsedNumbers,
-                owner:buyerWalletAddress
+                owner: buyerWalletAddress
             }
-            const data = JSON.stringify({data: raw_data});
-            const event = await fetch('/api/tickets/newTicket',{
-                method:'POST',
+            const data = JSON.stringify({ data: raw_data });
+            const event = await fetch('/api/tickets/newTicket', {
+                method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body:data
+                body: data
             })
-            
-            return {error:null,data:parsedNumbers, status: 'success', message: 'Ticket bought successfully.'} //data = todos los tickets comprados del usuario que lo compro
-        }catch(err){
-            return {error:err,data:null, status: 'danger', message: 'Error while buying ticket.'}
+
+            return { error: null, data: parsedNumbers, status: 'success', message: 'Ticket bought successfully.' } //data = todos los tickets comprados del usuario que lo compro
+        } catch (err) {
+            return { error: err, data: null, status: 'danger', message: 'Error while buying ticket.' }
         }
     }
 
-    async ownerWithdrawAmount(receiver_address,ticketEventAddress){
-        const event_contract = new ethers.Contract(ticketEventAddress,this.#ABIticket,this.#signer);
-        try{
+    async ownerWithdrawAmount(receiver_address, ticketEventAddress) {
+        const event_contract = new ethers.Contract(ticketEventAddress, this.#ABIticket, this.#signer);
+        try {
             const amount = await event_contract.amountTickets();
             await event_contract.withdrawAmount(receiver_address);
-            return {error:null,data:'success',amount}
-        }catch(err){
-            return {error:err,data:null}
+            return { error: null, data: 'success', amount }
+        } catch (err) {
+            return { error: err, data: null }
         }
     }
 
-    async setTicketUsed(ticketEventAddress,ticketNumber,_usedDateString){
-        const event_contract = new ethers.Contract(ticketEventAddress,this.#ABIticket,this.#signer);
-        try{
+    async setTicketUsed(ticketEventAddress, ticketNumber, _usedDateString) {
+        const event_contract = new ethers.Contract(ticketEventAddress, this.#ABIticket, this.#signer);
+        try {
             //check if it was alr used 
             const ticket = await event_contract.ticketData(ticketNumber);
-            if(ticket.setUsedAdmin===true){
-                return {error:"ticket have been already used",data:null};
+            if (ticket.setUsedAdmin === true) {
+                return { error: "ticket have been already used", data: null };
             }
             //set it used at contract
-            await event_contract.setTicketUsed(ticketNumber,_usedDateString);
+            await event_contract.setTicketUsed(ticketNumber, _usedDateString);
             //set it used at database
             const raw_data = {
                 ticketEventAddress,
                 ticketNumber,
-                usedDate:_usedDateString
+                usedDate: _usedDateString
             }
-            const data = JSON.stringify({data: raw_data});
-            const event = await fetch('/api/tickets/setTicketUsed',{
-                method:'POST',
+            const data = JSON.stringify({ data: raw_data });
+            const event = await fetch('/api/tickets/setTicketUsed', {
+                method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body:data
+                body: data
             })
-           return {error:null,data:'success'};
-        }catch(err){
-            return {error:err,data:null}
+            return { error: null, data: 'success' };
+        } catch (err) {
+            return { error: err, data: null }
         }
     }
 
-    async checkUserOwnNftTicket(onlineEventAddress,userWalletAddress){
-        console.log('addressEvent',onlineEventAddress)
+    async checkUserOwnNftTicket(onlineEventAddress, userWalletAddress) {
+        console.log('addressEvent', onlineEventAddress)
         console.log(this.#signer)
-        const event_contract = new ethers.Contract(onlineEventAddress,this.#ABIticket,this.#signer);
+        const event_contract = new ethers.Contract(onlineEventAddress, this.#ABIticket, this.#signer);
         const userNftBalance = await event_contract.balanceOf(userWalletAddress);
         return userNftBalance;
     }
@@ -210,7 +223,7 @@ class useContracts {
 
     delay(seconds) {
         return new Promise(resolve => {
-          setTimeout(resolve, seconds * 1000);
+            setTimeout(resolve, seconds * 1000);
         });
     }
 }
