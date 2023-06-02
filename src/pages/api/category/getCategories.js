57644e08d84dfb4efe22123770835b8565ed73e8
InigoZepeda <inigis@hotmail.com>
2023-06-01 23:16:33 -0600

    4MB fix again

--- src/pages/api/category/getCategories.js
+++ src/pages/api/category/getCategories.js
@@ -1,8 +1,3 @@
-// export default async function handler(req, res) {
-//     //-----------COMPLETAR-----------
-//     res.status(200).json({data:'succes'})
-// }
-
 import connectMongo from '@/server/mongo';
 import Category from '@/server/models/category';
 
